3c48632678fefcd6fba927ef6def122b600098eb
Aksel Uhr <akseluhr8@gmail.com>
2022-08-18 11:44:20 +0200

    Added links + head

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import dynamic from "next/dynamic";
 import { useState } from "react";
+import Link from 'next/link';
+import Head from 'next/head';
 
 const HelloComponent = dynamic(() => import("../components/hello"), {
   loading: () => <b>Loading...</b>,
@@ -9,11 +11,18 @@ export default function Home() {
   const [show, setShow] = useState(false);
   return (
     <div>
+      <Head>
+        <title>Homep</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       Home component
       <div>{show ? <HelloComponent /> : null}</div>
       <div>
         <button onClick={() => setShow((pV) => !pV)}>{!show ? 'Show' : 'Hide'}</button>
       </div>
+      <button> 
+        <Link href="/posts/first-post">Post Here</Link>
+      </button>
     </div>
   );
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,25 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+
 export default function FirstPost() {
-    return <h1>First Post</h1>;
-  }
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+        <link rel="icon" href="/favicon.ico" />
+        {/* Third party sources is added here with <script src='' /> tag */}
+      </Head>
+      <h1>First Post</h1>
+      <Image
+        src='/images/aki GH.png'
+        height={150}
+        width={150}
+        alt='Aksel Uhr'
+      />
+      <button>
+        <Link href='/'>Back to home</Link>
+      </button>
+    </>
+  );
+}
